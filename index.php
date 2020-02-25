e486c9feaedce06d7188a752effa1969aafa9b31
Chihyu <chihyu10@gmail.com>
2020-02-25 09:58:42 +0100

    fixed form

--- index.php
+++ index.php
@@ -4,5 +4,22 @@ ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
-require 'connection.php';
-require 'insert.php';
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening();
+
+$connechi = new connectChi();
+$connechi->openConnection();
+require 'controller/homeController.php';
+
+
